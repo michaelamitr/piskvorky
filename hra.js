c6e0fc7938418412f2830b53213eb9f74232175e
Michaela Trčková <misa.trckova@volny.cz>
2023-04-07 16:07:25 +0200

    final

--- hra.js
+++ hra.js
@@ -3,14 +3,14 @@ let currentPlayer = 'circle';
 const classAddCircle = (event) => {
   event.target.classList.add('board__field--circle');
   const img = document.querySelector('#nowplays');
-  img.src = 'cross.svg';
+  img.src = 'cross-green.svg';
   event.target.disabled = true;
 };
 
 const classAddCross = (event) => {
   event.target.classList.add('board__field--cross');
   const img = document.querySelector('#nowplays');
-  img.src = 'circle.svg';
+  img.src = 'circle-purple.svg';
   event.target.disabled = true;
 };
 
